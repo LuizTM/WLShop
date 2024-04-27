a7d4c9789449320b510f640d97cae439e4ef8942
Luiz Tadeu <luiz.tml@hotmail.com>
2024-04-27 18:04:52 -0300

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WLShop"
+include(":app")
+include(":wlshop")
+include(":shared:sa-analytics")
+include(":shared:sa-network")
+include(":shared:sa-data")
